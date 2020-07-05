4db08b39af0c82b0b811f116285d1afc5c2e0917
tijnvlith <66628263+tijnvlith@users.noreply.github.com>
2020-07-05 17:38:02 +0200

    Add files via upload

---
+++ 001/index.js
@@ -0,0 +1,24 @@
+
+console.log("Hello Winc Academy")
+
+let myName = 'Tyn';
+
+console.log("myName")
+
+let if = 'undefined';
+
+let four = 4;
+
+four + four
+
+4 + 4
+
+5 / 4
+
+7 - 6
+
+8 * 7
+
+4 % 1
+
+let age = 22;
